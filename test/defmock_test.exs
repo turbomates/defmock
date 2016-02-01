281043613bbe383b713e632777d31fd6c62acf4a
driver <driver.devel@gmail.com>
2016-02-01 18:10:45 +0300

    Use separated module for tests

--- test/defmock_test.exs
+++ test/defmock_test.exs
@@ -1,44 +1,46 @@
 defmodule DefmockTest do
   use ExUnit.Case
-  use Defmock
   doctest Defmock
-  defmock test_method: 1, test_method: 0, test_method: 3
+
+  defmodule API do
+    use Defmock
+
+    defmock test_method: 1, test_method: 0, test_method: 3
+  end
+
 
   setup_all do
-    start_link
+    API.start_link
     :ok
   end
 
   setup do
-    on_exit fn ->
-      clear
-    end
-    :ok
+    on_exit(fn -> API.clear end)
   end
 
   test "method_name in atom without arity" do
-    mock(:test_method, {:ok, %{}})
-    {:ok, %{}} = test_method
+    API.mock(:test_method, {:ok, %{}})
+    {:ok, %{}} = API.test_method
   end
 
   test "method_name in list without arity" do
-    mock([test_method: 0], {:ok, %{}})
-    {:ok, %{}} = test_method
+    API.mock([test_method: 0], {:ok, %{}})
+    {:ok, %{}} = API.test_method
   end
 
   test "method_name in list with 1 arity" do
-    mock([test_method: 1], fn([first_v: :test]) -> {:ok, %{}} end)
-    {:ok, %{}} = test_method([first_v: :test])
+    API.mock([test_method: 1], fn([first_v: :test]) -> {:ok, %{}} end)
+    {:ok, %{}} = API.test_method([first_v: :test])
   end
 
   test "list method_names" do
-    mock([test_method: 0, test_method: 3], {:ok, %{}})
-    mock([test_method: 1], fn(m) -> {:ok, %{value: m}} end)
-    {:ok, %{value: :first_v}} = test_method(:first_v)
-    {:ok, %{}} = test_method()
+    API.mock([test_method: 0, test_method: 3], {:ok, %{}})
+    API.mock([test_method: 1], fn(m) -> {:ok, %{value: m}} end)
+    {:ok, %{value: :first_v}} = API.test_method(:first_v)
+    {:ok, %{}} = API.test_method
   end
 
   test "mock is not set" do
-    {:error, {:mock_error, _}} = test_method
+    {:error, {:mock_error, _}} = API.test_method
   end
 end
